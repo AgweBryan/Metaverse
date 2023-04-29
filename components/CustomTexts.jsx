3bfdea9cfa84d98890843bc867dfad01a8b81801
AgweBryan <agwebryanmunakwa@gmail.com>
2023-04-29 13:41:59 +0100

    add explore section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import styles from "../styles";
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textVariant2, textContainer } from "../utils/motion";
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -17,4 +17,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold text-white md:text-[64px] text-[40px] ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+);
